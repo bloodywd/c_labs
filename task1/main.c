c9168330e0350c3a13399626d20d86f3391e93b1
Степуков Евгений Вячеславович <stepukov@cs.karelia.ru>
2024-09-18 14:34:07 +0300

    init

---
+++ task1/main.c
@@ -0,0 +1,17 @@
+/**
+ * main.c -- программа "Hello, students!"
+ *
+ * Copyright (c) 2024, Evgeny Stepukov <stepukov@cs.petrsu.ru>
+ *
+ * This code is licensed under MIT license.
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    /* Выводим приветствие */
+    fprintf(stdout, "Hello, students!\n");
+
+    return 0;
+}
